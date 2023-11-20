47d49a48480f08c8e5f53fd1fc7a7d3a83634cd4
D-Sriver <duez.s@proton.me>
2023-11-20 10:45:57 +0100

    :sparkles: Fermeture de la modale.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -14,10 +14,20 @@ const formData = document.querySelectorAll(".formData");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//TODO: fermer la modale
+modalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+//TODO: fermer la modale
+function closeModal() {
+  if (modalbg.style.display === "block") {
+    document.querySelector(".close").addEventListener("click", function () {
+      modalbg.style.display = "none";
+    });
+  }
 }
 
 
