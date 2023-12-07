03b918626279985ac872a4e4fda1a52fb8bf549b
D-Sriver <duez.s@proton.me>
2023-12-07 16:07:20 +0100

    :construction: Re factorisation complete

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,102 +1,4 @@
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const formFields = document.querySelectorAll('.text-control, #birthdate, #quantity, [name="location"], #checkbox1');
-const form = document.getElementById("form");
-const confirmationMessage = document.createElement("div");
-
-const errorMessages = {
-	first: "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
-	last: "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
-	email: "Veuillez remplir une adresse e-mail valide.",
-	birthdate: "Veuillez sélectionner une date de naissance.",
-	quantity: "Vous devez choisir une option.",
-	location: "Vous devez choisir une option.",
-	checkbox1: "Vous devez vérifier que vous acceptez les termes et conditions."
-};
-/// Event Listeners ///
-// ouvre la modale si le bouton est cliqué
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-// ferme la modale si le bouton est cliqué
-modalBtn.forEach((btn) => btn.addEventListener("click", closeModal));
-//change la couleur du bouton de soumission si les champs sont valides
-formFields.forEach(field => field.addEventListener('input', updateButtonColor));
-formFields.forEach(field => field.addEventListener('input', () => {
-	updateButtonColor();
-	updateValidationMessages();
-}));
-
-/// Functions ///
-// Fonction qui ouvre la modale
-function launchModal() {
-	if (submitBtn.disabled) {
-		// Si le bouton est désactivé, vérifier les champs et afficher les messages d'erreur
-		updateValidationMessages();
-	} else {
-		modalbg.style.display = "block";
-	}
-}
-
-// Fonction qui ferme la modale par la croix
-function closeModal() {
-	if (modalbg.style.display === "block") {
-		document.querySelector(".close").addEventListener("click", function () {
-			modalbg.style.display = "none";
-		});
-	}
-}
-
-// Fonction qui change la couleur du bouton de soumission
-function updateButtonColor() {
-	const isValid = validate();
-	const submitBtn = document.getElementById("submitBtn");
-//si le formulaire est valide, changez la couleur du bouton en rouge
-	if (isValid) {
-		submitBtn.style.backgroundColor = "#ff0000";
-		submitBtn.addEventListener("click", submitForm); // Ajoutez cet événement au bouton de soumission
-// si pas la couleur reste grise
-	} else {
-		submitBtn.style.backgroundColor = "#e7e7e7";
-		submitBtn.removeEventListener("click", submitForm); // Supprimez cet événement du bouton de soumission
-
-	}
-}
-
-function resetForm() {
-	form.reset();
-}
-
-// Fonction appelée lors de la soumission du formulaire
-function submitForm(event) {
-	event.preventDefault();
-	//affiche le message de confirmation dans la console
-	console.log("Formulaire soumis");
-	//recupere les données du formulaire dans une constante formData
-	const formData = {
-		first: document.getElementById("first").value,
-		last: document.getElementById("last").value,
-		email: document.getElementById("email").value,
-		birthdate: document.getElementById("birthdate").value,
-		quantity: document.getElementById("quantity").value,
-		location: document.querySelector('input[name="location"]:checked').value,
-		checkbox1: document.getElementById("checkbox1").checked
-	};
-	// Affiche les données du formulaire dans la console sous forme de tableau
-	console.table(formData);
-	//cache tout le formulaire dans la modale
-	document.getElementById("form").style.display = "none";
-	// remplace le formulaire par le message de confirmation dans la modale
-	document.getElementById("confirmationSection").style.display = "block";
-	document.body.appendChild(confirmationMessage);
-	// cache le message de confirmation après 4 secondes
-	setTimeout(function () {
-		modalbg.style.display = "none";
-	}, 4000);
-	//reset le formulaire
-	resetForm();
-}
-
+// Fonction pour basculer la topnav en topnav responsive
 function editNav() {
 	var x = document.getElementById("myTopnav");
 	if (x.className === "topnav") {
@@ -106,64 +8,187 @@ function editNav() {
 	}
 }
 
-// Fonction qui vérifie les champs du formulaire et affiche les messages d'erreur
-function updateValidationMessages() {
-	formFields.forEach(field => {
-		const container = field.closest(".formData");
-		const fieldName = field.id;
-		const errorDiv = container.querySelector('.error-message');
-		// Supprimer les messages existants
-		errorDiv?.remove();
-//si le champ est vide, ajoutez un message d'erreur
-		if (field.value.trim() === "") {
-			container.setAttribute("data-error", errorMessages[fieldName]);
-		} else {
-			//si le champ n'est pas vide, supprimez le message d'erreur
-			container.removeAttribute("data-error");
-			container.setAttribute("data-error-visible", "true");
+// Sélection des éléments dans le DOM et création de constantes
+const texteRemerciement = document.querySelector(".text-label");
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const first = document.getElementById("first");
+const last = document.getElementById("last");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const locationsErreur = document.getElementById("location1");
+const checkbox1 = document.getElementById("checkbox1");
+const submitBouton = document.querySelector(".btn-submit");
+
+// Ajout d'EventListener aux boutons
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.addEventListener("click", closeModal);
+submitBouton.addEventListener("click", SubmitForm);
+
+// Fonction pour lancer la modal
+function launchModal() {
+	// Affiche la modal
+	modalbg.style.display = "block";
+	displayElements();
+}
+
+// Fonction pour fermer la modal
+function closeModal() {
+	// Ferme la modal
+	modalbg.style.display = "none";
+	// Reset le formulaire
+	resetForm();
+}
+
+// Fonction pour soumettre le formulaire au clic du bouton
+function SubmitForm() {
+	if (submitBouton.value === "Fermer") {
+		closeModal();
+		resetForm();
+	} else {
+		if (validate()) {
 		}
+	}
+}
+
+// Fonction pour vérifier les formData du formulaire
+function displayElements() {
+	formData.forEach(element => {
+		element.style.display = "block";
 	});
 }
 
-// Fonction qui vérifie si le formulaire est valide
-function validate() {
-	const firstName = document.getElementById("first").value;
-	const lastName = document.getElementById("last").value;
-	const email = document.getElementById("email").value;
-	const birthDate = document.getElementById("birthdate").value;
-	const quantity = document.getElementById("quantity").value;
-	const locationRadios = document.getElementsByName("location");
-	let locationSelected = false;
-	const checkbox1 = document.getElementById("checkbox1").checked;
-// Regex pour vérifier l'adresse e-mail
-	let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-// Vérifiez que tous les champs sont remplis
-	switch (true) {
-		case !emailRegex.test(email.trim()):
-		case firstName.trim() === "":
-		case lastName.trim() === "":
-		case birthDate.trim() === "":
-			return false;
-		// Verifie que la date de naissance est valide
-		default:
-			const birthDateObj = new Date(birthDate);
-			const eighteenYearsAgo = new Date();
-			eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
+// Fonction pour réinitialiser le formulaire
+function resetForm() {
+	// Réinitialisation des valeurs des champs
+	first.value = "";
+	last.value = "";
+	email.value = "";
+	birthdate.value = "";
+	quantity.value = "";
+	displayElements();
 
-			switch (true) {
-				case birthDateObj > eighteenYearsAgo:
-				case quantity.trim() === "" || isNaN(quantity):
-					return false;
-				// Vérifiez que l'une des options de localisation est sélectionnée
-				default:
-					for (let i = 0; i < locationRadios.length; i++) {
-						if (locationRadios[i].checked) {
-							locationSelected = true;
-							break;
-						}
-					}
-					// Vérifiez que les conditions acceptées
-					return checkbox1;
-			}
+	// Réinitialisation du texte de remerciement et du bouton de soumission
+	texteRemerciement.textContent = "À quel tournoi souhaitez-vous participer cette année ?";
+	texteRemerciement.style.padding = "0";
+	texteRemerciement.style.fontSize = "17px";
+	texteRemerciement.style.textAlign = "justify";
+	submitBouton.value = "C'est parti";
+}
+
+// Fonction pour valider le champ du formulaire avec regex
+function validateField(value, minLength, errorMessage, element, regex) {
+	if (value.length < minLength || !regex.test(value)) {
+		// Affiche le message d'erreur
+		element.closest("div").setAttribute("data-error-visible", true);
+		element.closest("div").setAttribute("data-error", errorMessage);
+		return false;
+	} else {
+		// Efface le message d'erreur
+		element.closest("div").setAttribute("data-error-visible", false);
+		element.closest("div").removeAttribute("data-error");
+		return true;
 	}
+}
+
+// Fonction pour valider l'ensemble du formulaire
+function validate() {
+	// Utilisation de regex pour le nom/prénom et e-mail
+	const nameRegex = /^[A-Za-zéèêëîïôöùûü\s-]+$/;
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+	const errors = [];
+
+	if (!validateField(first.value, 2, "Veuillez entrer un prénom valide (sans chiffres).", first, nameRegex)) {
+		errors.push("first");
+	}
+	if (!validateField(last.value, 2, "Veuillez entrer un nom valide (sans chiffres).", last, nameRegex)) {
+		errors.push("last");
+	}
+	if (!validateField(email.value, 1, "Veuillez entrer un e-mail valide.", email, emailRegex)) {
+		errors.push("email");
+	}
+	if (!validateBirthdate()) {
+	}
+	if (!validateQuantity()) {
+	}
+	if (!validateLocation()) {
+	}
+	if (!validateCheckbox()) {
+	}
+
+	// Afficher tous les messages d'erreur en même temps
+	errors.forEach(error => {
+		document.getElementById(error).closest("div").setAttribute("data-error-visible", true);
+	});
+
+	return errors.length === 0; // Retourne vrai s'il n'y a aucune erreur
+}
+
+// Fonction pour valider la date de naissance
+function validateBirthdate() {
+	let Anniv = new Date(birthdate.value);
+	let YearAnniv = Anniv.getFullYear();
+	if (YearAnniv > 1900 && YearAnniv < 2023) {
+		birthdate.closest("div").setAttribute("data-error-visible", false);
+		return true;
+	} else {
+		birthdate.closest("div").setAttribute("data-error-visible", true);
+		birthdate.closest("div").setAttribute("data-error", "Vous devez entrer votre date de naissance.");
+		return false;
+	}
+}
+
+// Fonction pour valider le nombre de tournois
+function validateQuantity() {
+	let quantityvalid = /^[0-9]{1,2}$/;
+	if (quantityvalid.test(quantity.value)) {
+		quantity.closest("div").setAttribute("data-error-visible", false);
+		return true;
+	} else {
+		quantity.closest("div").setAttribute("data-error-visible", true);
+		quantity.closest("div").setAttribute("data-error", "Veuillez entrer un chiffre valide.");
+		return false;
+	}
+}
+
+// Fonction pour valider le radio bouton
+function validateLocation() {
+	let AllLocations = document.querySelectorAll('input[name="location"]');
+	let OneLocation = Array.prototype.slice.call(AllLocations).some((x) => x.checked);
+	if (OneLocation === false) {
+		locationsErreur.closest("div").setAttribute("data-error-visible", true);
+		locationsErreur.closest("div").setAttribute("data-error", "Vous devez choisir une option");
+		return false;
+	} else {
+		locationsErreur.closest("div").setAttribute("data-error-visible", false);
+		return true;
+	}
+}
+
+// Fonction pour valider les conditions d'utilisation
+function validateCheckbox() {
+	if (checkbox1.checked === false) {
+		checkbox1.closest("div").setAttribute("data-error-visible", true);
+		checkbox1.closest("div").setAttribute("data-error", "Vous devez vérifier que vous acceptez les termes et conditions.");
+		return false;
+	} else {
+		checkbox1.closest("div").setAttribute("data-error-visible", false);
+		return true;
+	}
+}
+
+// Fonction pour afficher et créer la modale de validation
+function ValidationModal() {
+	for (let i = 0; i < formData.length; i++) {
+		formData[i].style.display = "none";
+	}
+	texteRemerciement.textContent = "Merci pour votre inscription !";
+	texteRemerciement.style.padding = "30% 25px";
+	texteRemerciement.style.fontSize = "30px";
+	texteRemerciement.style.textAlign = "center";
+	submitBouton.value = "Fermer";
 }
